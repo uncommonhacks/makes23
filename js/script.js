c77972ce5c6ce1cbf09e7af5a8cc4090185c11aa
Clarice Kim <claricek@uchicago.edu>
2023-10-30 17:38:41 -0500

    added mobile

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 window.onload = function() {
+    var w = window.innerWidth;
+
+    if ((w < 1100)) {
+        window.location.href = "https://makes2023.uncommonhacks.com/";
+    }
     // Scroll to the bottom of the page
     window.scrollTo(0, document.body.scrollHeight);
 }
