7cd3d88d3e4c10bdc3f473c5240f7c88c73ab2ca
Clarice Kim <claricek@uchicago.edu>
2023-10-30 17:49:23 -0500

    fixed typo

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ window.onload = function() {
     var w = window.innerWidth;
 
     if ((w < 1100)) {
-        window.location.href = "https://makes2023.mobile.uncommonhacks.com/";
+        window.location.href = "https://makes23.mobile.uncommonhacks.com/";
     }
     // Scroll to the bottom of the page
     window.scrollTo(0, document.body.scrollHeight);
