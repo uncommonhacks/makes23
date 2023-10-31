3af685549a2a942541dc598b725dd486fad9a19c
Hung Le <conghungletran@gmail.com>
2023-10-30 22:55:14 -0500

    signup & thankyou pages

---
+++ js/signup.js
@@ -0,0 +1,31 @@
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+var forms = document.querySelectorAll(".needs-validation");
+
+var url =
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        if (form.checkValidity()) {
+          console.log("validated form");
+          fn = $("input[name=fname]").val();
+          ln = $("input[name=lname]").val();
+          yr = $("input[name=year]:checked").val();
+          te = $("input[name=team]:checked").val();
+          ws = $("input[name=workshop]:checked").val();
+          ss = $("input[name=spaceship]:checked").val();
+          console.log(fn, ln, yr, te, ws, ss);
+          URL = `https://docs.google.com/forms/d/e/1FAIpQLScjpflyFahjeBqTC-g4_2i-LRtufjJeZ06XyFh5_Laewhoa5w/formResponse?&submit=Submit?usp=pp_url&entry.1985883040=${fn}&entry.999432858=${ln}&entry.141446247=${yr}&entry.1297437449=${te}&entry.524434191=${ws}&entry.276209187=${ss}`;
+
+          var wdw = window.open(URL);
+          wdw.close();
+          window.location.href = "thankyou.html";
+        }
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
