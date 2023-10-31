06d704bbc50eec6dc98196c199a7a9239e12a94f
Anton <antonoutkine@gmail.com>
2023-10-31 13:41:30 -0500

    Add additional check for when the iframe loads

--- js/signup.js
+++ js/signup.js
@@ -20,11 +20,14 @@ var url =
           console.log(fn, ln, yr, te, ws, ss);
           URL = `https://docs.google.com/forms/d/e/1FAIpQLScjpflyFahjeBqTC-g4_2i-LRtufjJeZ06XyFh5_Laewhoa5w/formResponse?&submit=Submit?usp=pp_url&entry.1985883040=${fn}&entry.999432858=${ln}&entry.141446247=${yr}&entry.1297437449=${te}&entry.524434191=${ws}&entry.276209187=${ss}`;
 
+          const iframe = document.getElementById("frame")
           //   var wdw = window.open(URL);
-          document.getElementById("frame").setAttribute("src", URL);
-          setTimeout(() => {
-              window.location.href = "thankyou.html";
-          }, 500);
+          iframe.setAttribute("src", URL);
+          iframe.onload = () => {
+            setTimeout(() => {
+                window.location.href = "thankyou.html";
+            }, 500);
+          }
         }
         form.classList.add("was-validated");
       },
